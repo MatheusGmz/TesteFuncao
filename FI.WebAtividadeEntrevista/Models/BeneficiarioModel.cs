93b7d72543a7ca821a1aff9c0a4b55e1b61d0668
Matheus <matheus.santos@minutoseguros.com.br>
2020-07-07 16:48:40 -0300

    First commit

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAtividadeEntrevista.Models
+{
+    public class BeneficiarioModel
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string  CPF { get; set; }
+        public long IdCliente { get; set; }
+
+    }
+}
